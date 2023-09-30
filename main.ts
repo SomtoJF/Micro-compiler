fe28e60eadef8733d157f10182408855148d4c9e
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-09-30 11:01:44 +0100

    Initial commit

---
+++ main.ts
@@ -0,0 +1,31 @@
+let obj: any = {
+    type: 'Program',
+    body: [
+        {
+            type: 'CallExpression',
+            name: 'add',
+            params: [
+                {
+                    type: 'NumberLiteral',
+                    value: '2',
+                },
+                {
+                    type: 'CallExpression',
+                    name: 'subtract',
+                    params: [
+                        {
+                            type: 'NumberLiteral',
+                            value: '4',
+                        },
+                        {
+                            type: 'NumberLiteral',
+                            value: '2',
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+};
+
+console.log(obj);
